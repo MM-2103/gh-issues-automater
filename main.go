25752f88848575dba6efec737c3df0388e7ef678
MM-2103 <mohsen.menem@protonmail.com>
2025-01-16 22:20:03 +0100

    added keywords array

--- main.go
+++ main.go
@@ -8,11 +8,12 @@ import (
 func main() {
 	//DONE: Run the shell command
 	//DONE: Loop over it so it get executed multiple times
-	//TODO: Add a array of keywords
+	//DONE: Add a array of keywords
 	//TODO: Loop over the array of keywords so that they are used
 	//TODO: Make the keywords array fill in the placeholders
 
 	for i := 0; i < 10; i++ {
+		titles := [3]string{"dashboard", "team", "permissions"}
 		cmd := exec.Command("gh", "issue", "create", "-t", "test", "-b", "test2")
 
 		out, err := cmd.Output()
