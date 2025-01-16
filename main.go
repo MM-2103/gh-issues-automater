4f2cd3fc3cf296f5ef3c9951a7958a648257e329
MM-2103 <mohsen.menem@protonmail.com>
2025-01-16 22:13:20 +0100

    Looping so it get run multiple times, Done

--- main.go
+++ main.go
@@ -7,17 +7,19 @@ import (
 
 func main() {
 	//DONE: Run the shell command
-	//TODO: Loop over it so it get executed multiple times
+	//DONE: Loop over it so it get executed multiple times
 	//TODO: Add a array of keywords
 	//TODO: Loop over the array of keywords so that they are used
 	//TODO: Make the keywords array fill in the placeholders
 
-	cmd := exec.Command("gh", "issue", "create", "-t", "%s", "-b", "test2")
+	for i := 0; i < 10; i++ {
+		cmd := exec.Command("gh", "issue", "create", "-t", "test", "-b", "test2")
 
-	out, err := cmd.Output()
-	if err != nil {
-		fmt.Println("could not run command: ", err)
+		out, err := cmd.Output()
+		if err != nil {
+			fmt.Println("could not run command: ", err)
+		}
+
+		fmt.Println(string(out))
 	}
-
-	fmt.Println(string(out))
 }
