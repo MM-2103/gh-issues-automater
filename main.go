eec7df1daaceb52e69ae213efc71bebb76584254
MM-2103 <mohsen.menem@protonmail.com>
2025-04-16 09:29:38 +0200

    feat: implement configurable issue creation with CRUD support via JSON

--- main.go
+++ main.go
@@ -1,34 +1,103 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 	"os/exec"
 )
 
+type Config struct {
+	Repo     string              `json:"repo"`
+	Keywords map[string]CrudOpts `json:"keywords"`
+}
+
+type CrudOpts struct {
+	Create bool `json:"create"`
+	Read   bool `json:"read"`
+	Update bool `json:"update"`
+	Delete bool `json:"delete"`
+}
+
 func main() {
-	//DONE: Run the shell command
-	//DONE: Loop over it so it get executed multiple times
-	//DONE: Add a array of keywords
-	//DONE: Loop over the array of keywords so that they are used
-	//DONE: Make the keywords array fill in the placeholders
-	//TODO: Possible 4 stories per subject (ie: dashboard create, read, update, delete)
-	//TODO: CRUD or no CRUD
-	//TODO: JSON file with the subject and the option to do CRUD or not
-	//TODO: Possibility to execute the command with flags instead of passing JSON
-
-	titles := []string{"dashboard", "team", "permissions"}
-
-	for _, title := range titles {
-		// Als VAR wil ik VAR2 omdat VAR3
-		cmd := exec.Command("gh", "issue", "create", "-t", title, "-F", ".github/ISSUE_TEMPLATE/issue-template.md")
-
-		output, err := cmd.CombinedOutput()
-		if err != nil {
-			fmt.Printf("Error creating issue with title %s: %s\n", title, err)
-			continue
-		}
-
-		fmt.Printf("Output for title %s: %s\n", title, string(output))
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: issue-creator <config-file.json>")
+		os.Exit(1)
 	}
 
+	configFile := os.Args[1]
+	config, err := loadConfig(configFile)
+	if err != nil {
+		fmt.Printf("Error loading config: %v\n", err)
+		os.Exit(1)
+	}
+
+	for keyword, crudOpts := range config.Keywords {
+		if hasCRUD(crudOpts) {
+			createCRUDIssues(config.Repo, keyword, crudOpts)
+		} else {
+			createBaseIssue(config.Repo, keyword)
+		}
+	}
+}
+
+func loadConfig(path string) (*Config, error) {
+	file, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	if err := json.Unmarshal(file, &config); err != nil {
+		return nil, err
+	}
+
+	if config.Repo == "" {
+		return nil, fmt.Errorf("repository not specified in config")
+	}
+
+	return &config, nil
+}
+
+func hasCRUD(opts CrudOpts) bool {
+	return opts.Create || opts.Read || opts.Update || opts.Delete
+}
+
+func createCRUDIssues(repo, keyword string, opts CrudOpts) {
+	crudOperations := map[string]bool{
+		"aanmaken":    opts.Create,
+		"lezen":       opts.Read,
+		"updaten":     opts.Update,
+		"verwijderen": opts.Delete,
+	}
+
+	for operation, enabled := range crudOperations {
+		if enabled {
+			title := fmt.Sprintf("Als dev-team wil ik %s kunnen %s", keyword, operation)
+			createIssue(repo, title)
+		}
+	}
+}
+
+func createBaseIssue(repo, keyword string) {
+	title := fmt.Sprintf("Als dev-team wil ik %s hebben", keyword)
+	createIssue(repo, title)
+}
+
+func createIssue(repo, title string) {
+	body := ".github/ISSUE_TEMPLATE/issue-template.md"
+
+	cmd := exec.Command("gh", "issue", "create",
+		"--repo", repo,
+		"--title", title,
+		"-F", body,
+	)
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Printf("Error creating issue '%s': %v\nOutput: %s\n", title, err, string(output))
+		return
+	}
+
+	fmt.Printf("Successfully created issue: %s\n", title)
 }
